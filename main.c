79a3b5d2dfe2a10ad38d15746ed9cb0d2ab411e4
leverkuehn <nikita25t@yandex.ru>
2021-01-22 21:07:34 +0300

    The story begins

--- main.c
+++ main.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
+
+void printArray(int *a, int count)
+{
+	printf("[ ");
+	for (size_t i = 0; i < count - 1; i++)
+		prinf("%d, ");
+	printf(" %d]\n", a[count - 1]);
+}
+
 int main()
 {
+	printf("Hello!\n");
+
 	return 0;
 }
